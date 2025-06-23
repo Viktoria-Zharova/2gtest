6d07d047b8b7044637eff67352c05735d2aa9c8c
Herman <german.makarkin@gmail.com>
2025-06-23 13:53:26 +0300

    ARCH1-7683 feat: add popups

--- src/index.js
+++ src/index.js
@@ -166,7 +166,89 @@ const realtyScene = [
           pitch: 0.001,
           zoom: 19.5,
           rotation: -5,
-        }
+        },
+        poiGroups: [
+          {
+            id: 1003,
+            type: 'primary',
+            minZoom: 18.5,
+            elevation: 20,
+            fontSize: 15,
+            fontColor: '#3a3a3a',
+            data: [
+              {
+                coordinates: [37.383675, 55.808440],
+                label: '2Е\n42.94 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=13741977-8c48-49cb-bd5d-2603d84fa19a',
+                },
+              },
+              {
+                coordinates: [37.383570, 55.808457],
+                label: '2Е\n40.82 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=ed4e9c6e-0031-47b3-bd69-aa838567ee78',
+                },
+              },
+              {
+                coordinates: [37.383800, 55.808414],
+                label: '2Е\n35.57 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=508703e7-3bcd-4bfd-a8e8-639053d1a2d5',
+                },
+              },
+              {
+                coordinates: [37.383900, 55.808400],
+                label: '3Е\n50.15 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=61a2e758-1fdf-4cdf-aa51-a5ff75fbd46c',
+                },
+              },
+              {
+                coordinates: [37.384030, 55.808375],
+                label: '3Е\n0.88 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=d6580778-8d4f-4661-ac2f-9331d21080bd',
+                },
+              },
+              {
+                coordinates: [37.384030, 55.808327],
+                label: '2Е\n40.81 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=9620cfd3-f991-4718-9e88-dc044ddad7e9',
+                },
+              },
+              {
+                coordinates: [37.383925, 55.808327],
+                label: '2Е\n31.59 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=a4e22138-8e4a-4e6f-8922-4f32d04b8337',
+                },
+              },  
+              {
+                coordinates: [37.383855, 55.808337],
+                label: '1С\n22.09 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=48b24648-c3cd-4a5a-addc-d2eb84f7a572',
+                },
+              }, 
+              {
+                coordinates: [37.383650, 55.808370],
+                label: '1С\n21.72 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=080ca561-b0a4-4ef5-8b29-af670c1926b5',
+                },
+              },
+              {
+                coordinates: [37.383550, 55.808390],
+                label: '3Е\n50.84 м²',
+                userData: {
+                  url: 'https://samolet-2.widget.floor-plan.online/beta/?id=1b9642e1-6972-4975-a331-2367b1834bae',
+                },
+              },
+            ],
+          },
+        ],
       },
       {
         id: '2',
@@ -177,7 +259,124 @@ const realtyScene = [
           pitch: 0.001,
           zoom: 20,
           rotation: -5,
-        }
+        },
+        poiGroups: [
+          {
+            id: 1003,
+            type: 'primary',
+            minZoom: 18.5,
+            elevation: 1,
+            fontSize: 10,
+            fontColor: '#3a3a3a',
+            data: [
+              {
+                coordinates: [37.383100, 55.808540],
+                label: '121.41 м²\nТанцевальный зал / \nТренажерный зал',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503500',
+                },
+              },
+              {
+                coordinates: [37.382930, 55.808560],
+                label: '25.3 м²\nОбщественный туалет',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503498',
+                },
+              },
+              {
+                coordinates: [37.383050, 55.808500],
+                label: '131.38 м²\nКоридор',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503496',
+                },
+              },
+              {
+                coordinates: [37.383230, 55.808500],
+                label: '3.73 м²\nКофеточка',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503497',
+                },
+              },
+              {
+                coordinates: [37.383675, 55.808440],
+                label: '42.94 м²\nСалон красоты /\nМагазин кресел',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503501',
+                },
+              },
+              {
+                coordinates: [37.383570, 55.808457],
+                label: '40.82 м²\nБарбершоп /\nБиблиотека',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503501',
+                },
+              },
+              {
+                coordinates: [37.383800, 55.808414],
+                label: '35.57 м²\nРемонтная мастерская /\nМагазин одежды',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503507',
+                },
+              },
+              {
+                coordinates: [37.383965, 55.808390],
+                label: '101.03 м²\nКомпьютерный клуб /\nМедклиника',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503504',
+                },
+              },
+              {
+                coordinates: [37.384030, 55.808327],
+                label: '40.81 м²\nУчебный класс /\nДетский клуб',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503510',
+                },
+              },   
+              {
+                coordinates: [37.383925, 55.808327],
+                label: '31.59 м²\nКабинет психолога /\nКофейня',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503503',
+                },
+              },  
+              {
+                coordinates: [37.383855, 55.808337],
+                label: '22.09 м²\nWildberries /\nСДЭК',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503502',
+                },
+              }, 
+              {
+                coordinates: [37.383740, 55.808370],
+                label: '80.8 м²\nКоридор',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503505',
+                },
+              },
+              {
+                coordinates: [37.383650, 55.808370],
+                label: '21.72 м²\nOzon /\nЯндекс Маркет',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503506',
+                },
+              },
+              {
+                coordinates: [37.383550, 55.808390],
+                label: '50.84 м²\nСтоматология /\nСПАт',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503509',
+                },
+              },
+              {
+                coordinates: [37.383220, 55.808440],
+                label: '287.78 м²\nОфис /\nПятерочка',
+                userData: {
+                  url: 'https://samolet-new2.widget.hart-estate.ru/beta/?crmPlanId=503499',
+                },
+              },
+            ],
+          },
+        ],
       },
     ]
   }
